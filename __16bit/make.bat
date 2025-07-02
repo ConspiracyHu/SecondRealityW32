5ed110b565fdd5b7a83ecb2f338cfde0d038196d
Gargaj <gargaj@conspiracy.hu>
2025-07-02 14:08:28 +0200

    remove accidental test junk

--- __16bit/make.bat
+++
@@ -1,2 +0,0 @@
-"c:\Program Files (x86)\NASM\nasm.exe" d:\code\SecondRealityW32\__16bit\test16.asm -o d:\code\SecondRealityW32\__16bit\test16.com
-"c:\Program Files (x86)\DOSBox.mmx\dosbox-x.exe" "d:\code\SecondRealityW32\__16bit\test16.com" -noautoexec -nopromptfolder -exit
